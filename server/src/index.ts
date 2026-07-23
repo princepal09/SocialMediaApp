b099c1eeab42a0030fc9c3d097631d5f81702365
Prince Pal <princepal262501@gmail.com>
2026-07-23 19:56:56 +0530

    fix(server): correct formatting and improve server error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,21 @@
 import { app } from "./app.js";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import dbConnect from "./config/db.js";
 import { error } from "console";
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
-dbConnect
+dbConnect()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running on port ${PORT}`);
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    server.on("error", (error) => {
+      console.error("Server Error", error);
+      process.exit(1);
     });
   })
   .catch((err: any) => {
-    console.error("❌ MongoDB connection failed:", err);
+    console.error("MongoDB connection failed:", err);
   });

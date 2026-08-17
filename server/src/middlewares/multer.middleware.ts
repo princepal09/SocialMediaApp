3e3a14c3b7be1cbc12263767ff0b05b661516946
Prince Pal <princepal262501@gmail.com>
2026-08-17 17:43:05 +0530

    fix some bug on multer middleware

--- server/src/middlewares/multer.middleware.ts
+++ server/src/middlewares/multer.middleware.ts
@@ -1,13 +1,24 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadPath = path.resolve("public/temp");
+
+console.log("Upload path:", uploadPath);
+
+// Create temp folder if missing
+fs.mkdirSync(uploadPath, { recursive: true });
 
 const storage = multer.diskStorage({
-    destination : function(req, file, cb){
-        cb(null, "./public/temp")
+    destination: function (req, file, cb) {
+        cb(null, uploadPath);
     },
 
-    filename : function(req, file, cb){
-        cb(null, file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
     }
-})
+});
 
-export const upload = multer ({storage})
+export const upload = multer({
+    storage
+});
